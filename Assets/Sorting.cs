8e7de74a1e36c9b243108fc09cc0bc0c44712eeb
Shawn Bartlett <shawnkbartlett@gmail.com>
2026-02-23 04:35:42 -0800

    Add: Bars sort with lerp animation

--- Assets/Sorting.cs
+++ Assets/Sorting.cs
@@ -5,30 +5,45 @@ using UnityEngine;
 public class Sorting : MonoBehaviour
 {   
     public GameObject barPrefab;
+    public GameObject[] bars;
     public Transform bar;
     int [] numbers = new int[10];
 
     // Start is called before the first frame update
     void Start()
     {
-        
-        for (int x = 0; x < numbers.Length; x++)
-        {
-            numbers[x] = Random.Range(1,6);
-            
-
-            GameObject bar = Instantiate(barPrefab);
-            
-            bar.transform.localScale = new Vector3(bar.transform.localScale.x, numbers[x], bar.transform.localScale.z);
-            bar.transform.position = new Vector3(x * 0.3f, numbers[x] / 2f, 0);
-        }
-
-        
+        buildStartingBars();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+            System.Array.Sort(numbers); // FIXME: Temporary 
+        }
         
+        for (int x = 0; x < numbers.Length; x++){
+        
+            float y = Mathf.Lerp(bars[x].transform.localScale.y, numbers[x], 3f * Time.deltaTime);
+
+            bars[x].transform.localScale = new Vector3(bar.transform.localScale.x, y, bars[x].transform.localScale.z);
+            bars[x].transform.position = new Vector3(x * 0.3f, y / 2f, 0);
+        }
+    }
+
+    void buildStartingBars()
+    {
+        bars = new GameObject[numbers.Length];
+        // FIXME: Refactor into clean methods later
+        for (int x = 0; x < numbers.Length; x++)
+        {
+            numbers[x] = Random.Range(1,6);
+        
+             bars[x] = Instantiate(barPrefab);
+            
+            bars[x].transform.localScale = new Vector3(bars[x].transform.localScale.x, numbers[x], bars[x].transform.localScale.z);
+            bars[x].transform.position = new Vector3(x * 0.3f, numbers[x] / 2f, 0);
+        }
     }
 }
